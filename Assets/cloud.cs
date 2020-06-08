d58d7d5b58f4599bfe1b63980aae8e5cc928d210
fememism <git@koro.games>
2020-06-09 01:48:54 +0300

    бекграунд и меню

---
+++ Assets/cloud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+    SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        transform.position += Vector3.right/200;
+        if (transform.position.x>24)
+        {
+
+            transform.position = new Vector2(-24,Random.Range(3.0f,5.0f));
+            float scale = Random.Range(0.5f, 0.7f);
+            transform.localScale = new Vector2(scale, scale);
+            if (transform.position.y > 3) sr.sortingOrder = -11;
+            if (transform.position.y > 3.7f) sr.sortingOrder = -12;
+            if (transform.position.y > 4.4f) sr.sortingOrder = -13;
+
+        }
+    }
+}
